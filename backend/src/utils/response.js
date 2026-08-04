9fc143e411cfbd58ec5451ad1181a2edc6861dc0
Darshana Bhandari <bandaridarshana@gmail.com>
2026-08-04 23:12:11 +0545

    Update authentication system

--- backend/src/utils/response.js
+++ backend/src/utils/response.js
@@ -1,15 +1,16 @@
-export const sucessResponse = (res, data, message = null,statuscode=200) => {
-  return res.status(statuscode).json({
-    success: true,
-    message: message,
-    data: data,
-  });
-}
-
-export const errorResponse=(res,message,errors=null,statuscode=500)=>{
-    return res.status(statuscode).json({
-        success: false,
-        message: message,
-        errors: errors  
+ export const successResponse=(res,message,data=null,statusCode=200)=>{
+    return res.status(statusCode).json({
+        success:true,
+        message,
+        data
     })
 }
+
+
+   export const errorResponse=(res,message,errors=null,statusCode=500)=>{
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        errors
+    })
+  }
