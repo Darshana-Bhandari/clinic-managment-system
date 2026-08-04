ed0b65e79b5bc991bfd0a3c1fdb18221246e4e05
Darshana Bhandari <bandaridarshana@gmail.com>
2026-08-04 23:37:42 +0545

    Update Prisma schema and database migration

--- backend/prisma/migrations/20260802131616_init/migration.sql
+++ backend/prisma/migrations/20260802131616_init/migration.sql
@@ -1,21 +1,16 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `fullName` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` ENUM('ADMIN', 'DOCTOR', 'STAFF', 'PATIENT') NOT NULL DEFAULT 'PATIENT',
+    `isActive` BOOLEAN NOT NULL DEFAULT true,
+    `creaedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
-  - You are about to drop the column `name` on the `user` table. All the data in the column will be lost.
-  - A unique constraint covering the columns `[phone]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `fullname` to the `User` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `phone` to the `User` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `user` DROP COLUMN `name`,
-    ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    ADD COLUMN `fullname` VARCHAR(191) NOT NULL,
-    ADD COLUMN `isActive` BOOLEAN NOT NULL DEFAULT true,
-    ADD COLUMN `phone` VARCHAR(191) NOT NULL,
-    ADD COLUMN `role` ENUM('ADMIN', 'DOCTOR', 'STAFF', 'PATIENT') NOT NULL DEFAULT 'PATIENT',
-    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
-
--- CreateIndex
-CREATE UNIQUE INDEX `User_phone_key` ON `User`(`phone`);
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_phone_key`(`phone`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
