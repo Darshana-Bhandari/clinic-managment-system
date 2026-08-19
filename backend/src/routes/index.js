799348f4e95511139d2fd888641925a3f46a761c
Darshana Bhandari <bandaridarshana@gmail.com>
2026-08-19 22:33:50 +0545

    feat: add department module and update backend

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,19 +1,24 @@
-import express from 'express';
+import express from "express"
+import authRoutes from "../module/auth/auth.routes.js"
+import adminRoutes from "../module/auth/admin.routes.js"
+import patientRoutes from "../module/patient/patient.routes.js"
+import doctorRoutes from "../module/doctor/doctor.routes.js"
+import appointmentRoutes from "../module/appointment/appointent.routes.js"
+import departmentRoutes from "../module/department/department.routes.js"
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  return res.json({
-    success: true,
-    message: 'Clinic Management API is running',
-  });
-});
+router.get("/health",(req,res)=>{
+    return res.json({
+        message:"cilnic managemnet system",
+        success:true
+    })
+})
+router.use("/auth",authRoutes)
+router.use("/admin",adminRoutes)
+router.use("/patient",patientRoutes)
+router.use("/doctor",doctorRoutes)
+router.use("/appointment",appointmentRoutes)
+router.use("/department",departmentRoutes)
 
-router.get('/health', (req, res) => {
-  return res.json({
-    success: true,
-    message: 'Clinic management system',
-  });
-});
-
-export default router;
+export default router
